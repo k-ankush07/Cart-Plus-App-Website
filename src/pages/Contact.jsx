3e08025fb7bc4a1e71da699f261b3d8b53126904
k-ankush07 <ankush@ecombithub.com>
2026-05-29 00:30:14 +0530

    working contact page banner

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import ContactHeading from '../sections/Contact/ContactHeading'
 import ContactForm from '../sections/Contact/ContactForm'
+
 export default function Contact() {
   return (
     <div>
+      <ContactHeading />
       <ContactForm />
     </div>
   )
--- src/sections/Contact/ContactForm.jsx
+++ src/sections/Contact/ContactForm.jsx
@@ -1,12 +1,7 @@
 import React from 'react'
-import Container from '../../components/Container'
 
 export default function ContactForm() {
-    return (
-        <section className='relative'>
-            <Container className='relative z-10 py-[40px] max-[540px]:py-[25px]'>
-
-            </Container>
-        </section>
-    )
+  return (
+    <div>ContactForm</div>
+  )
 }
