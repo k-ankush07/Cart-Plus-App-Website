14a2444bbc82b42d451e1e768e27097496334dc2
k-ankush07 <ankush@ecombithub.com>
2026-05-29 03:58:28 +0530

    working articles create filesArticlesPage

--- src/pages/Articles.jsx
+++ src/pages/Articles.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import TopHeading from '../components/TopHeading'
+import ArticlesPage from '../sections/Articles/ArticlesPage'
 
 export default function Articles() {
-  return (
-    <div>
-        
-    </div>
-  )
+    return (
+        <div>
+            <TopHeading
+                heading='How To Reduce Cart Abandonment on Shopify'
+                paragraph='Discover proven strategies Shopify brands use to reduce abandoned carts, improve checkout completion, and increase revenue with a better cart experience.'
+            />
+            <ArticlesPage />
+        </div>
+    )
 }
