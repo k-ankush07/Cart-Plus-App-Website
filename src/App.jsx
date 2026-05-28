a360ad797f5efca9a3383564f9b5e49a5f6d9b13
k-ankush07 <ankush@ecombithub.com>
2026-05-28 22:15:51 +0530

    changes app.js file

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,9 @@ export default function App() {
       <Routes>
         <Route element={<Layout />}>
           <Route path="/" element={<Home />} />
-           <Route path="/about" element={<About />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/pricing" element={<Pricing />} />
+          <Route path="*" element={<Home />} />
           {/* <Route path="/features" element={<Features />} />
           <Route path="/pricing" element={<Pricing />} />
           <Route path="/about" element={<About />} />
