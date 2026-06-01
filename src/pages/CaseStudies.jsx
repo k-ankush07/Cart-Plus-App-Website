6d282f16c8a846d4f90ce63ca9acffb4226f865d
k-ankush07 <ankush@ecombithub.com>
2026-06-01 10:45:32 +0530

    done case studio pags

--- src/pages/CaseStudies.jsx
+++ src/pages/CaseStudies.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import CartStudio from '../sections/CaseStudies/CartStudio'
+import Blog from '../sections/Blog/Blog';
+import FAQ from '../sections/home/FAQ'
+import CTA from '../sections/home/CTA'
 
 export default function CaseStudies() {
     return (
         <div>
             <CartStudio />
+            <Blog />
+            <FAQ />
+            <CTA />
         </div>
     )
 }
--- src/sections/CaseStudies/CartStudio.jsx
+++ src/sections/CaseStudies/CartStudio.jsx
@@ -1,9 +1,37 @@
 import React from 'react'
+import Container from '../../components/Container'
 
 export default function CartStudio() {
-  return (
-    <div>
-        
-    </div>
-  )
+    return (
+        <section className='relative'>
+            <Container className='py-[40px] max-[540px]:py-[25px] relative z-10'>
+                <div className='grid grid-cols-1 md:grid-cols-2 items-center gap-[20px]'>
+                    <div className='flex flex-col relative z-10'>
+                        <h1 className='font-semibold text-[50px] lg:text-[65px] max-[540px]:text-[40px] max-[540px]:leading-[45px] leading-[55px] lg:leading-[70px] max-w-none md:max-w-[660px]'>
+                            Turn More Carts Into More Revenue
+                        </h1>
+                        <p className='text-[18px] max-[540px]:text-[16px] max-w-none md:max-w-[600px] py-[20px] max-[540px]:py-[16px]'>
+                            See how Shopify brands are increasing average order value, boosting conversions, and generating more revenue with HubCart's smart cart drawer, upsells, and checkout optimization tools.
+                        </p>
+                        <p className='text-[18px] max-[540px]:text-[16px] max-w-none md:max-w-[600px] py-[20px] max-[540px]:py-[16px]'>
+                            This aligns perfectly with your homepage messaging and feels more original than copying Aftersell's wording.
+                        </p>
+                    </div>
+                    <div className='w-full'>
+                        <img
+                            src='https://hubsyntax.com/cart-images/Rectangle 240648575.svg'
+                            alt="HubCart"
+                            fetchPriority="high"
+                            loading="eager"
+                            decoding="async"
+                            className='w-full relative z-20'
+                        />
+                    </div>
+                </div>
+            </Container>
+            <div className='absolute right-0 top-0 hidden lg:block'>
+                <img src="https://hubsyntax.com/cart-images/Mask group (111).svg" alt="" loading="lazy" decoding="async" />
+            </div>
+        </section>
+    )
 }
