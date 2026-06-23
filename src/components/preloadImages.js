9adeb1a666411e71b620c5fa2542c9e3b616cbbc
k-ankush07 <ankush@ecombithub.com>
2026-06-23 17:36:29 +0530

    update index.html

--- src/components/preloadImages.js
+++ src/components/preloadImages.js
@@ -3,6 +3,8 @@ const preloadImages = [
   "https://cartplus.io/cartplus-img/Group 1707480308.svg",
   "https://cartplus.io/cartplus-img/Group 1707480310.svg",
   "https://cartplus.io/cartplus-img/CartLogo.svg",
+  "https://cartplus.io/cartplus-img/Group 1707480290 (1).svg",
+  
 ];
 
 export default preloadImages;
